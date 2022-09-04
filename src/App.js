b58e472c2a034f203ccfcc812fc0c07dd2df304f
Yahya Hafez <yhafez3@gmail.com>
2022-09-03 22:28:16 -0500

    add styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Navbar } from "./components";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 
+import "./App.scss";
+
 const App = () => {
     return (
         <div className="app">
