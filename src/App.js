23e86f8fa98c0a16fd3a5bf093382da00fa4eb84
Yahya Hafez <yhafez3@gmail.com>
2024-04-11 19:35:28 -0700

    chore: update portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React from 'react'
 
-import { Navbar } from "./components";
-import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from './components'
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
 
-import "./App.scss";
+import './App.scss'
 
 const App = () => {
-    return (
-        <div className="app">
-            <Navbar />
-            <Header />
-            <About />
-            <Work />
-            <Skills />
-            <Testimonial />
-            <Footer />
-        </div>
-    );
-};
+	return (
+		<div className='app'>
+			<Navbar />
+			<Header />
+			<About />
+			<Work />
+			<Skills />
+			<Testimonial />
+		</div>
+	)
+}
 
-export default App;
+export default App
